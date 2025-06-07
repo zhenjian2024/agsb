0364efa3fa2e66cda72a5b644dadeda1c10f5dff
zhenjian2024 <zhenjian2024@qq.com>
2025-06-07 22:12:57 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -32,10 +32,10 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 
 # ====== 全局可配置参数（可直接在此处修改） ======
 USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+UUID = "09851029-474e-4f96-bbb5-8763c5712b90"                     # UUID，留空则自动生成
+PORT = 12250                   # Vmess端口，留空或0则自动生成
+DOMAIN = "zhen.zhen0607.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiNTQyYzMyMjgxMDFiZjBmZWZkYjVlZjUzNWI1MTVlMGIiLCJ0IjoiNjMwZDA4OGUtZGIyMS00MjJkLThhODAtZWEzMDkwYTU0ZDc1IiwicyI6IlpXVTJaVFU0T1RndE9ETXhPQzAwWm1aaExUZ3pOemd0TnpCbVpEVmpaRE0zWW1SayJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
